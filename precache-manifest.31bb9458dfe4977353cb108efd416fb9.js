2d48860c9016b2958190ab0a27e3e69c36c63c9c
straudi <mina789@naver.com>
2020-04-20 11:43:41 +0900

    Updates

---
+++ precache-manifest.31bb9458dfe4977353cb108efd416fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6c4fd9539440bc89780ee787bfff51",
+    "url": "/react_movie/index.html"
+  },
+  {
+    "revision": "04c4c30fb1459745ce99",
+    "url": "/react_movie/static/js/2.3b3565c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_movie/static/js/2.3b3565c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49465b97f0d2ee9417ac",
+    "url": "/react_movie/static/js/main.b494fa69.chunk.js"
+  },
+  {
+    "revision": "8935c872b98ad90e273a",
+    "url": "/react_movie/static/js/runtime-main.03d89e6d.js"
+  },
+  {
+    "revision": "31d26327470d3d7f96f7bed3417fe659",
+    "url": "/react_movie/static/media/logo.31d26327.svg"
+  }
+]);
